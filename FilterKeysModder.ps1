a878b55b80edc7cc8438326fb92a8faaf25ffaa5
Dr.LuSlower <148411728+LuSlower@users.noreply.github.com>
2024-07-25 02:53:06 -0500

    Add files via upload

--- FilterKeysModder.ps1
+++ FilterKeysModder.ps1
@@ -128,9 +128,43 @@ function Set-FilterKeys {
     [System.Runtime.InteropServices.Marshal]::FreeHGlobal($pFilterKeys)
 
     if ($success) {
-        [System.Windows.Forms.MessageBox]::Show("Teclas de Filtro actualizadas correctamente.", "Sucess", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+        [System.Windows.Forms.MessageBox]::Show("Filter keys updated", "Info", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
     } else {
-        [System.Windows.Forms.MessageBox]::Show("Error al actualizar las Teclas de Filtro.", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+        [System.Windows.Forms.MessageBox]::Show("Error updating Filter Keys", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+    }
+}
+
+function Get-RegistryValues {
+    param (
+        [string]$regPath
+    )
+
+    # Obtener los valores del registro con manejo de errores
+    try {
+        $values = Get-ItemProperty -Path $regPath -ErrorAction Stop
+
+        # Asignar los valores a las variables, o $null si no están presentes
+        $BounceTime = if ($values.PSObject.Properties["BounceTime"]) { $values.BounceTime } else { $null }
+        $DelayBeforeAcceptance = if ($values.PSObject.Properties["DelayBeforeAcceptance"]) { $values.DelayBeforeAcceptance } else { $null }
+        $AutoRepeatDelay = if ($values.PSObject.Properties["AutoRepeatDelay"]) { $values.AutoRepeatDelay } else { $null }
+        $AutoRepeatRate = if ($values.PSObject.Properties["AutoRepeatRate"]) { $values.AutoRepeatRate } else { $null }
+        $Flags = if ($values.PSObject.Properties["Flags"]) { $values.Flags } else { $null }
+
+        return @{
+            BounceTime = $BounceTime
+            DelayBeforeAcceptance = $DelayBeforeAcceptance
+            AutoRepeatDelay = $AutoRepeatDelay
+            AutoRepeatRate = $AutoRepeatRate
+            Flags = $Flags
+        }
+    } catch {
+        return @{
+            BounceTime = $null
+            DelayBeforeAcceptance = $null
+            AutoRepeatDelay = $null
+            AutoRepeatRate = $null
+            Flags = $null
+        }
     }
 }
 
@@ -138,77 +172,102 @@ function Set-FilterKeys {
 Console -Hide
 [System.Windows.Forms.Application]::EnableVisualStyles();
 $form = New-Object System.Windows.Forms.Form
-$form.ClientSize = New-Object System.Drawing.Size(180, 250)
+$form.ClientSize = New-Object System.Drawing.Size(180, 185)
 $form.MaximizeBox = $false
 $form.MinimizeBox = $false
 $form.Text = "FilterKeysModder"
 $form.StartPosition = "CenterScreen"
 $form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
 
-# Checkbox para habilitar las Teclas de Filtro
+# Obtener los valores
+$Values = Get-RegistryValues -regPath $regPath
+
+# FilterKeyssOn
 $chkEnable = New-Object System.Windows.Forms.CheckBox
 $chkEnable.Text = "On FilterKeys"
 $chkEnable.AutoSize = $true
-$chkEnable.Location = New-Object System.Drawing.Point(50, 20)
+$chkEnable.Location = New-Object System.Drawing.Point(50, 10)
 
-if ((Get-ItemProperty -Path $regPath -Name "Flags" -ErrorAction Stop).Flags -ne 0) {
+if ($Values.Flags -ne $null) {
     $chkEnable.Checked = $true
 }
 
 $form.Controls.Add($chkEnable)
 
-# campos de datos para los tiempos
+# BounceTime
 $lblBounceTime = New-Object System.Windows.Forms.Label
 $lblBounceTime.Text = "BounceTime (ms):"
-$lblBounceTime.Location = New-Object System.Drawing.Point(10, 60)
+$lblBounceTime.Location = New-Object System.Drawing.Point(12, 40)
 $form.Controls.Add($lblBounceTime)
 
 $txtBounceTime = New-Object System.Windows.Forms.TextBox
-$txtBounceTime.Location = New-Object System.Drawing.Point(115, 58)
+$txtBounceTime.Location = New-Object System.Drawing.Point(115, 38)
 $txtBounceTime.Width = 50
-$txtBounceTime.Text = (Get-ItemProperty -Path $regPath -Name "BounceTime").BounceTime
+$txtBounceTime.Text = $Values.BounceTime
 $form.Controls.Add($txtBounceTime)
 
+# DelayTime
 $lblDelayTime = New-Object System.Windows.Forms.Label
 $lblDelayTime.Text = "DelayTime (ms):"
-$lblDelayTime.Location = New-Object System.Drawing.Point(10, 100)
+$lblDelayTime.Size = New-Object System.Drawing.Size(90, 15)
+$lblDelayTime.Location = New-Object System.Drawing.Point(20, 70)
 $form.Controls.Add($lblDelayTime)
 
 $txtDelayTime = New-Object System.Windows.Forms.TextBox
-$txtDelayTime.Location = New-Object System.Drawing.Point(115, 98)
+$txtDelayTime.Location = New-Object System.Drawing.Point(115, 68)
 $txtDelayTime.Width = 50
-$txtDelayTime.Text = (Get-ItemProperty -Path $regPath -name "DelayBeforeAcceptance").DelayBeforeAcceptance
+$txtDelayTime.Text = $Values.DelayTime
 $form.Controls.Add($txtDelayTime)
 
+# RepeatDelay
 $lblRepeatDelay = New-Object System.Windows.Forms.Label
 $lblRepeatDelay.Text = "RepeatDelay (ms):"
-$lblRepeatDelay.Location = New-Object System.Drawing.Point(10, 140)
+$lblRepeatDelay.Location = New-Object System.Drawing.Point(10, 100)
 $form.Controls.Add($lblRepeatDelay)
 
 $txtRepeatDelay = New-Object System.Windows.Forms.TextBox
-$txtRepeatDelay.Location = New-Object System.Drawing.Point(115, 138)
+$txtRepeatDelay.Location = New-Object System.Drawing.Point(115, 98)
 $txtRepeatDelay.Width = 50
-$txtRepeatDelay.Text = (Get-ItemProperty -Path $regPath -name "AutoRepeatDelay").AutoRepeatDelay
+$txtRepeatDelay.Text = $Values.RepeatDelay
 $form.Controls.Add($txtRepeatDelay)
 
+# RepeatRate
 $lblRepeatRate = New-Object System.Windows.Forms.Label
 $lblRepeatRate.Text = "RepeatRate (ms):"
-$lblRepeatRate.Location = New-Object System.Drawing.Point(10, 180)
+$lblRepeatRate.Location = New-Object System.Drawing.Point(15, 130)
 $form.Controls.Add($lblRepeatRate)
 
 $txtRepeatRate = New-Object System.Windows.Forms.TextBox
-$txtRepeatRate.Location = New-Object System.Drawing.Point(115, 178)
+$txtRepeatRate.Location = New-Object System.Drawing.Point(115, 128)
 $txtRepeatRate.Width = 50
-$txtRepeatRate.Text = (Get-ItemProperty -Path $regPath -name "AutoRepeatRate").AutoRepeatRate
+$txtRepeatRate.Text = $Values.RepeatRate
 $form.Controls.Add($txtRepeatRate)
 
-$btnApply = New-Object System.Windows.Forms.Button
-$btnApply.Text = "Save"
-$btnApply.Location = New-Object System.Drawing.Point(50, 215)
-$form.Controls.Add($btnApply)
+# KeyPress event handlers
+function OnKeyPress {
+    param (
+        [System.Object]$sender, 
+        [System.Windows.Forms.KeyPressEventArgs]$e
+    )
+    if ($e.KeyChar -notmatch '[0-9]' -and $e.KeyChar -ne [char][System.Windows.Forms.Keys]::Back) {
+        $e.Handled = $true
+    }
+}
 
-# aplicar todos los cambios
-$btnApply.Add_Click({
+
+$txtBounceTime.Add_KeyPress({ OnKeyPress -sender $txtBounceTime -e $_ })
+$txtDelayTime.Add_KeyPress({ OnKeyPress -sender $txtDelayTime -e $_ })
+$txtRepeatDelay.Add_KeyPress({ OnKeyPress -sender $txtRepeatDelay -e $_ })
+$txtRepeatRate.Add_KeyPress({ OnKeyPress -sender $txtRepeatRate -e $_ })
+
+$btnSave = New-Object System.Windows.Forms.Button
+$btnSave.Text = "Save"
+$btnSave.Size = New-Object System.Drawing.Size(50, 20)
+$btnSave.Location = New-Object System.Drawing.Point(30, 155)
+$form.Controls.Add($btnSave)
+
+# Save
+$btnSave.Add_Click({
     $enableFilterKeys = if ($chkEnable.Checked) { $true } else { $false }
     $bounceTime = [int]$txtBounceTime.Text
     $delayTime = [int]$txtDelayTime.Text
@@ -218,6 +277,19 @@ $btnApply.Add_Click({
     Set-Flags -enable $enableFilterKeys
     Set-FilterKeys-Reg -bounceTime $bounceTime -delayTime $delayTime -repeatRate $repeatRate -repeatDelay $repeatDelay
     Set-FilterKeys -enable $enableFilterKeys -bounceTime $bounceTime -delayTime $delayTime -repeatRate $repeatRate -repeatDelay $repeatDelay
+})
+
+$btnReset = New-Object System.Windows.Forms.Button
+$btnReset.Text = "Reset"
+$btnReset.Size = New-Object System.Drawing.Size(50, 20)
+$btnReset.Location = New-Object System.Drawing.Point(100, 155)
+$form.Controls.Add($btnReset)
+
+# Reset
+$btnReset.Add_Click({
+    Set-Flags -enable $false
+    Remove-ItemProperty -Path $regPath -Name "*"
+    Set-FilterKeys -enable $false -bounceTime 0 -delayTime 0 -repeatRate 0 -repeatDelay 0
 })
 
 $form.ShowDialog()
